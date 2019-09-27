13298d69b063faea84cc401c162fbc193fbebf61
marreddy-python <marreddy@becknf.com>
2019-09-27 16:09:51 +0530

    fixed date picker

--- celery_task.py
+++ celery_task.py
@@ -14,7 +14,7 @@ app.config.update(
     CELERYBEAT_SCHEDULE = {
         'periodic_task-every-minute': {
             'task': 'periodic_task',
-            'schedule': crontab(minute="18",hour="14" ,day_of_week="mon,tue,wed,thu,fri")
+            'schedule': crontab(minute="0",hour="0" ,day_of_week="mon,tue,wed,thu,fri")
         }}
 )
 
